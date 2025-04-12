43cf053421bff67633bc67f1a0e0aeaf8f8282d7
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-04-12 21:13:08 +0700

    feat: add pertemuan2

---
+++ pertemuan2/index.php
@@ -0,0 +1,68 @@
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// echo "ini echo";
+// print "ini print";
+// print_r("ini print_r");
+// var_dump("ini var_dump");
+
+// echo 123;
+
+// echo true;
+
+// penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "dadan";
+
+// kutip dua " lebih sakti karena bisa detect variabel
+// echo "Halo, $nama";
+
+// Operator
+// Aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x + $y;
+// echo 1 + 1;
+
+// Penggabungan String / Concatenation / Concat
+// .
+// $nama_depan = "ahdan";
+// $nama_belakang = "firdaus";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x += 5;
+// echo $x;
+
+// $nama = "ahdan";
+// $nama .= " ";
+// $nama .= "firdaus";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// Identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 10;
+// var_dump($x < 20 && $x % 2 == 0);
+?>
