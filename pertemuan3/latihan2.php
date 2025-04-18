51972064ee50d6b7a38c3bf743d2b08e8071ec89
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-04-18 14:31:55 +0700

    feat: add pertemuan3

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,37 @@
+<?php 
+// pengkondisian
+// if, else, else if
+// &&, ||, !
+// ternary
+// switch
+
+// $x = 20;
+// $y = 20;
+
+// if( $x < $y ) {
+//   echo "benar";
+// } else if( $x == $y ) {
+//   echo "bingo!";
+// } 
+// else {
+//   echo "salah";
+// }
+
+// $dua = 2;
+// echo $dua == 2 ? "benar" : "salah";
+
+// switch( $dua ) {
+//   case 1:
+//     echo "satu";
+//     break;
+//   case 2:
+//     echo "dua";
+//     break;
+//   case 3:
+//     echo "tiga";
+//     break;
+//   default:
+//     echo "salah";
+//     break;
+// }
+?>
