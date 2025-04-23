65003accf6d638fa4d656d5c9fdd4d493f05b56f
AhdanFirdaus <muhammadahdanf1@gmail.com>
2025-04-23 15:02:45 +0700

    feat: add pertemuan16

---
+++ pertemuan16/login.php
@@ -0,0 +1,66 @@
+<?php 
+session_start();
+if(isset($_SESSION['login'])) {
+  header('Location: index.php');
+  exit;
+}
+require "functions.php";
+
+if(isset($_POST["login"])) {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  $result = mysqli_query($conn, "
+    SELECT * FROM user WHERE username = '$username'
+  ");
+
+  // cek username
+  if(mysqli_num_rows($result) === 1) {
+    // cek password 
+    $row = mysqli_fetch_assoc($result);
+    if(password_verify($password, $row["password"])) {
+      // set session
+      $_SESSION["login"] = true;
+      header("Location: index.php");
+      exit;
+    }
+  }
+  $error = true;
+}
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Halaman Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="d-flex justify-content-center align-items-center vh-100 bg-light">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6 col-lg-4">
+        <div class="card shadow-sm p-4">
+          <h2 class="text-center mb-4">Halaman Login</h2>
+          <?php if(isset($error)) :?>
+            <p class="text-danger text-center"><em>username / password salah</em></p>
+          <?php endif;?>
+          <form action="" method="post">
+            <div class="mb-3">
+              <label for="username" class="form-label">Username:</label>
+              <input type="text" class="form-control" id="username" name="username" required>
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Password:</label>
+              <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <button type="submit" name="login" class="btn btn-primary w-100">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
